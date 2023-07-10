81ce191aa36887b0b9cf324094b4668b53b3ec2d
CaterinaMD <kaximaxmd@hotmail.com>
2023-07-10 19:29:50 -0300

    cambio generacion de zip

--- generar.sh
+++ generar.sh
@@ -22,11 +22,9 @@ for i in $(seq "$cantidad");
 do
 	archivo=$(cut -d "," -f 1 Nombres.txt | shuf -n 1)
 	curl -o "${archivo}.jpeg" "https://thispersondoesnotexist.com/"
-	
+
 	# echo "Comprimimos archivo cambiando su tamaño"
-	# ls -l		Para verificar que se comprime
 	# convert "${archivo}.jpeg" -gravity center -resize 512x512+0+0 -extent 512x512 "${archivo}.jpeg"
-	# ls -l		Para verificar que se comprime
 
 	echo "Generamos checksum ${archivo}_suma_verificacion.txt"
 	sha256sum "$archivo.jpeg" > "${archivo}_suma_verificacion.txt"
@@ -41,15 +39,15 @@ do
 		echo "Error. Checksum incorrecto"
 		exit 1
 	fi
-	
+
 	echo "Se ha creado \"${archivo}.jpeg\""
-	
-	zip imagen.zip "${archivo}_suma_verificacion.txt"
+
+	zip suma_v.zip "${archivo}_suma_verificacion.txt"
 	zip imagen.zip "${archivo}.jpeg"
-	
+
 	rm "${archivo}_suma_verificacion.txt"
 	rm "${archivo}.jpeg"
-	
+
 	sleep 2
 done
 
