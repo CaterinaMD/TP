d8e424dec19da83fc47aecf78604a44d450d8152
CaterinaMD <kaximaxmd@hotmail.com>
2023-07-12 14:30:49 -0300

    modificacion para agregar los archivos descomprimidos

--- procesar.sh
+++ procesar.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-datos=$(unzip imagen.zip -d temp_folder)
-imagenes=$(unzip -l imagen.zip | tail -n 4 | head -n -2 | cut -c 31-)
-
-if [ -e tamaño_im ];
+directorio="archivo_descomprimido"
+if [ -d archivo_descomprimido ];
 then
-	rm tamaño_im
-fi
-
-for archivo in $datos;
-do
-	imagen=${archivo:12}
-	if [[ $imagen =~ [A-Z][a-z]+_[A-Z][a-z]+\.jpeg  ]];
+	datos=$(ls "$directorio")
+	if [ -e tamaño_imagen.zip ];
 	then
-		echo "Modificando el tamaño de $imagen"
-		echo "archivo: $archivo"
-		echo "CUMPLE: $imagen"
-		arch="temp_folder/$imagen"
-		convert "${arch}" -gravity center -resize 512x512+0+0 -extent 512x512 "t_${imagen}"
-		zip tamaño_im "t_${imagen}"
+		rm tamaño_imagen.zip
 	fi
-done
-
-rm "t_"*
-rm -r temp_folder
+	echo "$datos"
+	for archivo in $datos;
+	do
+		if [[ $archivo =~ [A-Z][a-z]+_[A-Z][a-z]+\.jpeg  ]];
+		then
+			echo "Modificando el tamaño de $archivo"
+			arc="$directorio/$archivo"
+			convert "${arc}" -gravity center -resize 512x512+0+0 -extent 512x512 "t_$archivo"
+			zip tamaño_imagen.zip "t_$archivo"
+		else
+			echo "$archivo no cumple con los requisitos"
+		fi
+	done
+else
+        echo "Error. El archivo debe ser descomprimido anteriormente."
+        exit 1
+fi
+rm t_*
+exit 0
